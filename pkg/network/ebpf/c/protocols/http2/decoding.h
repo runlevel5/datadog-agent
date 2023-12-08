1f9a78eb0e2bab7ea8db3204111bb0921ccd1fba
Guy Arbitman <guy20495@gmail.com>
2023-12-08 14:32:19 +0200

    usm: http2: Remove http2_ctx as it is not needed anymore
    After the last 2 commits, the struct http2_ctx_t was just a wrapper to http2_stream_key_t.

--- pkg/network/ebpf/c/protocols/http2/decoding.h
+++ pkg/network/ebpf/c/protocols/http2/decoding.h
@@ -379,9 +379,9 @@ static __always_inline void process_headers_frame(struct __sk_buff *skb, http2_s
     process_headers(skb, dynamic_table_value, current_stream, headers_to_process, interesting_headers, http2_tel);
 }
 
-static __always_inline void parse_frame(struct __sk_buff *skb, skb_info_t *skb_info, conn_tuple_t *tup, http2_ctx_t *http2_ctx, dynamic_table_value_t *dynamic_table_value, struct http2_frame *current_frame, http2_telemetry_t *http2_tel) {
-    http2_ctx->http2_stream_key.stream_id = current_frame->stream_id;
-    http2_stream_t *current_stream = http2_fetch_stream(&http2_ctx->http2_stream_key);
+static __always_inline void parse_frame(struct __sk_buff *skb, skb_info_t *skb_info, conn_tuple_t *tup, http2_stream_key_t *http2_stream_key, dynamic_table_value_t *dynamic_table_value, struct http2_frame *current_frame, http2_telemetry_t *http2_tel) {
+    http2_stream_key->stream_id = current_frame->stream_id;
+    http2_stream_t *current_stream = http2_fetch_stream(http2_stream_key);
     if (current_stream == NULL) {
         return;
     }
@@ -395,7 +395,7 @@ static __always_inline void parse_frame(struct __sk_buff *skb, skb_info_t *skb_i
     bool is_rst = current_frame->type == kRSTStreamFrame;
     // If rst, and stream is empty (no status code, or no response) then delete from inflight
     if (is_rst && (current_stream->response_status_code == 0 || current_stream->request_started == 0)) {
-        bpf_map_delete_elem(&http2_in_flight, &http2_ctx->http2_stream_key);
+        bpf_map_delete_elem(&http2_in_flight, http2_stream_key);
         return;
     }
 
@@ -409,7 +409,7 @@ static __always_inline void parse_frame(struct __sk_buff *skb, skb_info_t *skb_i
     }
 
     if (should_handle_end_of_stream) {
-        handle_end_of_stream(current_stream, &http2_ctx->http2_stream_key, http2_tel);
+        handle_end_of_stream(current_stream, http2_stream_key, http2_tel);
     }
 
     return;
@@ -777,8 +777,8 @@ int socket__http2_frames_parser(struct __sk_buff *skb) {
     }
 
     const __u32 zero = 0;
-    http2_ctx_t *http2_ctx = bpf_map_lookup_elem(&http2_ctx_heap, &zero);
-    if (http2_ctx == NULL) {
+    http2_stream_key_t *http2_stream_key = bpf_map_lookup_elem(&http2_stream_key_heap, &zero);
+    if (http2_stream_key == NULL) {
         goto delete_iteration;
     }
 
@@ -796,10 +796,9 @@ int socket__http2_frames_parser(struct __sk_buff *skb) {
     http2_frame_with_offset *frames_array = tail_call_state->frames_array;
     http2_frame_with_offset current_frame;
 
-    // create the http2 ctx for the current http2 frame.
-    bpf_memset(http2_ctx, 0, sizeof(http2_ctx_t));
-    http2_ctx->http2_stream_key.tup = dispatcher_args_copy.tup;
-    normalize_tuple(&http2_ctx->http2_stream_key.tup);
+    bpf_memset(http2_stream_key, 0, sizeof(http2_stream_key_t));
+    http2_stream_key->tup = dispatcher_args_copy.tup;
+    normalize_tuple(&http2_stream_key->tup);
     dynamic_table_value->key.tup = dispatcher_args_copy.tup;
 
     #pragma unroll(HTTP2_FRAMES_PER_TAIL_CALL)
@@ -817,7 +816,7 @@ int socket__http2_frames_parser(struct __sk_buff *skb) {
 
         dispatcher_args_copy.skb_info.data_off = current_frame.offset;
 
-        parse_frame(skb, &dispatcher_args_copy.skb_info, &dispatcher_args_copy.tup, http2_ctx, dynamic_table_value, &current_frame.frame, http2_tel);
+        parse_frame(skb, &dispatcher_args_copy.skb_info, &dispatcher_args_copy.tup, http2_stream_key, dynamic_table_value, &current_frame.frame, http2_tel);
     }
 
     if (tail_call_state->iteration < HTTP2_MAX_FRAMES_ITERATIONS && tail_call_state->iteration < tail_call_state->frames_count) {
