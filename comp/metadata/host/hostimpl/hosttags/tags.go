b9d844edfc186ca38dd19b2f8c4395879d1174a6
Brian Floersch <brian.floersch@datadoghq.com>
2024-01-30 09:07:44 -0500

    linter

--- comp/metadata/host/hostimpl/hosttags/tags.go
+++ comp/metadata/host/hostimpl/hosttags/tags.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package hosttags provides access to host tags
 package hosttags
 
 import (
