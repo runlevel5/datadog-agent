0ff8429cfcd66386e68e665c4970d824fc9c9311
usamasaqib <usama.saqib@datadoghq.com>
2024-01-31 15:49:33 +0100

    conntrack compile time

--- pkg/network/tracer/compile_test.go
+++ pkg/network/tracer/compile_test.go
@@ -8,6 +8,7 @@
 package tracer
 
 import (
+	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,7 +21,26 @@ func TestConntrackCompile(t *testing.T) {
 	ebpftest.TestBuildMode(t, ebpftest.RuntimeCompiled, "", func(t *testing.T) {
 		cfg := config.New()
 		cfg.BPFDebug = true
+		startSelf := timeSyscall(syscall.RUSAGE_SELF)
+		startThread := timeSyscall(syscall.RUSAGE_THREAD)
+		startChildren := timeSyscall(syscall.RUSAGE_CHILDREN)
+		realStart := time.Now().UnixMicro()
+
 		out, err := getRuntimeCompiledConntracker(cfg)
+		realEnd := time.Now().UnixMicro()
+		endSelf := timeSyscall(syscall.RUSAGE_SELF)
+		endThread := timeSyscall(syscall.RUSAGE_THREAD)
+		endChildren := timeSyscall(syscall.RUSAGE_CHILDREN)
+		if startSelf != nil && startThread != nil && startChildren != nil && endSelf != nil && endThread != nil && endChildren != nil {
+			t.Logf("[Self] User time (sec): %d User time (usec): %d\n", endSelf.Utime.Sec-startSelf.Utime.Sec, endSelf.Utime.Usec-startSelf.Utime.Usec)
+			t.Logf("[Self] Sys time (sec): %d Sys time (usec): %d\n", endSelf.Stime.Sec-startSelf.Stime.Sec, endSelf.Stime.Usec-startSelf.Stime.Usec)
+			t.Logf("[Thread] User time (sec): %d User time (usec): %d\n", endThread.Utime.Sec-startThread.Utime.Sec, endThread.Utime.Usec-startThread.Utime.Usec)
+			t.Logf("[Thread] Sys time (sec): %d Sys time (usec): %d\n", endThread.Stime.Sec-startThread.Stime.Sec, endThread.Stime.Usec-startThread.Stime.Usec)
+			t.Logf("[Children] User time (sec): %d User time (usec): %d\n", endChildren.Utime.Sec-startChildren.Utime.Sec, endChildren.Utime.Usec-startChildren.Utime.Usec)
+			t.Logf("[Children] Sys time (sec): %d Sys time (usec): %d\n", endChildren.Stime.Sec-startChildren.Stime.Sec, endChildren.Stime.Usec-startChildren.Stime.Usec)
+			t.Logf("Real time: %d\n", realEnd-realStart)
+		}
+
 		require.NoError(t, err)
 		_ = out.Close()
 	})
