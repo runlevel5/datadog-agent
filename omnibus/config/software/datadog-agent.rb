22c3027c6aaba5a14b699134b6b3b4755b20e115
Sylvain Baubeau <lebauce@gmail.com>
2024-02-06 17:49:18 +0100

    Do not bundle process-agent into core agent

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -23,8 +23,7 @@ build do
 
   bundled_agents = []
   if linux_target?
-    bundled_agents = ["process-agent"]
-    bundled_agents << "security-agent" << "system-probe" unless heroku_target?
+    bundled_agents << "system-probe" << "security-agent" unless heroku_target?
   end
 
   # set GOPATH on the omnibus source dir for this software
