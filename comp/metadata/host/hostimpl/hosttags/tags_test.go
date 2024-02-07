6db57796f6cfb6a1d00d9589f05139ea93d2556a
Brian Floersch <brian.floersch@datadoghq.com>
2024-02-07 09:51:13 -0500

    lint

--- comp/metadata/host/hostimpl/hosttags/tags_test.go
+++ comp/metadata/host/hostimpl/hosttags/tags_test.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package hosttags contains the logic to collect host tags from various sources.
 package hosttags
 
 import (
