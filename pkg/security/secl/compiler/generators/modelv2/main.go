04bc84dcb8c50157a3c87d0328f1511f5e908440
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-02-13 15:52:23 +0100

    start emitting old model code

--- pkg/security/secl/compiler/generators/modelv2/main.go
+++ pkg/security/secl/compiler/generators/modelv2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/DataDog/datadog-agent/pkg/security/secl/compiler/generators/modelv2/parser"
 )
@@ -21,6 +22,42 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("%+v\n", tn)
+		emitOldModel(tn)
 	}
+}
+
+func emitOldModel(tn parser.TypeNode) {
+	w := os.Stdout
+
+	if tn.Doc != "" {
+		fmt.Fprintf(w, "// %s\n", tn.Doc)
+	}
+	fmt.Fprintf(w, "type %s struct {\n", tn.Name)
+
+	for _, field := range tn.Fields {
+		if field.IsEmbed {
+			fmt.Fprintf(w, "\t%s", field.Type)
+		} else {
+			fmt.Fprintf(w, "\t%s %s", field.Name, field.Type)
+		}
+
+		if len(field.SeclMappings) == 0 {
+			fmt.Fprintf(w, " `field:\"-\"`\n")
+		} else {
+			var fieldContent []string
+			for _, mapping := range field.SeclMappings {
+				field := mapping.Name
+				for k, v := range mapping.Options {
+					if k == "handler" || k == "opts" {
+						field += fmt.Sprintf(",%s:%s", k, v)
+					}
+				}
+				fieldContent = append(fieldContent, field)
+			}
+
+			fmt.Fprintf(w, " `field:\"%s\"`\n", strings.Join(fieldContent, ";"))
+		}
+	}
+
+	fmt.Fprintf(w, "}\n\n")
 }
