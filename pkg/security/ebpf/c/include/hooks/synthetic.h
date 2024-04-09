8c13d5743f75ae88840c3c33885f61257e1009d4
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-04-09 16:21:16 +0200

    store data in event

--- pkg/security/ebpf/c/include/hooks/synthetic.h
+++ pkg/security/ebpf/c/include/hooks/synthetic.h
@@ -1,7 +1,7 @@
 #ifndef _HOOKS_SYNTHETIC_H_
 #define _HOOKS_SYNTHETIC_H_
 
-#define PATH_SIZE 64
+#define PER_ARG_SIZE 64
 
 enum param_kind_t {
 	PARAM_NO_ACTION,
@@ -16,24 +16,36 @@ enum param_kind_t {
 	switch (param##idx##kind) { \
 	case PARAM_KIND_INTEGER: \
 		value = CTX_PARM##idx(ctx); \
-		bpf_printk("synthetic_hook, param%d `%d`", idx, value); \
+		bpf_probe_read(&event.data[(idx - 1) * PER_ARG_SIZE], sizeof(value), &value); \
 		break; \
 	case PARAM_KIND_NULL_STR: \
+		buf = &event.data[(idx - 1) * PER_ARG_SIZE]; \
 		path = (char *)CTX_PARM##idx(ctx); \
-		bpf_probe_read_str(&buf, PATH_SIZE, path); \
-		bpf_printk("synthetic_hook, param%d `%s`", idx, &buf); \
+		bpf_probe_read_str(buf, PER_ARG_SIZE, path); \
 		break; \
 	}
 
 HOOK_ENTRY("synthetic_hook")
 int hook_synthetic(ctx_t *ctx) {
-	char buf[PATH_SIZE];
-	char *path;
-	u64 value;
+	u64 synth_id;
+    LOAD_CONSTANT("synth_id", synth_id);
 
+	struct synthetic_event_t event = {
+		.synth_id = synth_id,
+	};
+
+	struct proc_cache_t *entry = fill_process_context(&event.process);
+    fill_container_context(entry, &event.container);
+    fill_span_context(&event.span);
+
+	char *path;
+	char *buf;
+	u64 value;
 
 	param_parsing(1);
 	param_parsing(2);
+
+	send_event(ctx, EVENT_SYNTHETIC, event);
 
     return 0;
 }
