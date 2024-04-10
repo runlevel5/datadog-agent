25d534df0173fe65195f4a55997554133ee159de
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-04-10 09:55:04 +0200

    prepare syscall hooking

--- pkg/security/ebpf/c/include/hooks/synthetic.h
+++ pkg/security/ebpf/c/include/hooks/synthetic.h
@@ -9,18 +9,18 @@ enum param_kind_t {
 	PARAM_KIND_NULL_STR,
 };
 
-#define param_parsing(idx) \
+#define param_parsing(PARM_PREFIX, idx) \
 	u64 param##idx##kind; \
     LOAD_CONSTANT("param" #idx "kind", param##idx##kind); \
                                              \
 	switch (param##idx##kind) { \
 	case PARAM_KIND_INTEGER: \
-		value = CTX_PARM##idx(ctx); \
+		value = PARM_PREFIX##idx(ctx); \
 		bpf_probe_read(&event.data[(idx - 1) * PER_ARG_SIZE], sizeof(value), &value); \
 		break; \
 	case PARAM_KIND_NULL_STR: \
 		buf = &event.data[(idx - 1) * PER_ARG_SIZE]; \
-		path = (char *)CTX_PARM##idx(ctx); \
+		path = (char *)PARM_PREFIX##idx(ctx); \
 		bpf_probe_read_str(buf, PER_ARG_SIZE, path); \
 		break; \
 	}
@@ -42,10 +42,38 @@ int hook_synthetic(ctx_t *ctx) {
 	char *buf;
 	u64 value;
 
-	param_parsing(1);
-	param_parsing(2);
+	param_parsing(CTX_PARM, 1);
+	param_parsing(CTX_PARM, 2);
 
 	send_event(ctx, EVENT_SYNTHETIC, event);
+
+    return 0;
+}
+
+HOOK_ENTRY("synthetic_syscall_hook")
+int hook_synthetic_syscall(ctx_t *ptctx) {
+	struct pt_regs *ctx = (struct pt_regs *) CTX_PARM1(ptctx);
+    if (!ctx) return 0;
+
+	u64 synth_id;
+    LOAD_CONSTANT("synth_id", synth_id);
+
+	struct synthetic_event_t event = {
+		.synth_id = synth_id,
+	};
+
+	struct proc_cache_t *entry = fill_process_context(&event.process);
+    fill_container_context(entry, &event.container);
+    fill_span_context(&event.span);
+
+	char *path;
+	char *buf;
+	u64 value;
+
+	param_parsing(SYSCALL64_PT_REGS_PARM, 1);
+	param_parsing(SYSCALL64_PT_REGS_PARM, 2);
+
+	send_event(ptctx, EVENT_SYNTHETIC, event);
 
     return 0;
 }
